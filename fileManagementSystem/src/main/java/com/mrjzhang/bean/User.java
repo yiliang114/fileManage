ccecb19061fb886f0cd6f37b9c05224d89d11899
zhijianzhang <mrjzhang@tencent.com>
2018-05-08 20:19:29 +0800

    feat: springboot restful api

---
+++ fileManagementSystem/src/main/java/com/mrjzhang/bean/User.java
@@ -0,0 +1,15 @@
+package com.mrjzhang.bean;
+
+public class User {
+    // 编号
+    private int id;
+    // 姓名
+    private String name;
+    // 年龄
+    private  int age;
+    // 构造函数
+    public  User() {
+
+    }
+
+}
