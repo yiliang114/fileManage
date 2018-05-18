bcc333c37c198b8bbc28180c69b8f2b956d473de
zhijianzhang <mrjzhang@tencent.com>
2018-05-18 22:14:20 +0800

    :tada: electron client gui

--- fileManage_web/src/routerMap.js
+++ fileManage_web/src/routerMap.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import {Router, Route, Switch} from 'react-router-dom';
+import {Router, Route, Switch,Link} from 'react-router-dom';
+import {Layout, Menu, Icon, Breadcrumb} from 'antd';
 import createHistory from 'history/createBrowserHistory';
 // 按路由拆分代码
 import Loadable from 'react-loadable';
 import MyLoadingComponent from './components/MyLoadingComponent'
+
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
 
 const history = createHistory();
 
@@ -33,14 +37,29 @@ class RouteMap extends React.Component {
   render() {
     return (
       <Router history={history}>
-        <Switch>
-          {/* <Route exact path="/" component={AsyncHome}/> */}
-
-          <Route exact path="/" component={AsyncElementTable}/>
-          <Route exact path="/city" component={AsyncCity}/>
-          <Route exact path="/monitor" component={AsyncMonitor}/>
-          <Route component={AsyncNotFound}/>
-        </Switch>
+        <Layout>
+          <Sider width={200} style={{ background: '#fff' }}>
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['monitor']}
+              style={{ height: '100%', borderRight: 0 }}
+            >
+              <Menu.Item key="monitor"><Link to="/monitor"><span><Icon type="user" />Element Monitor</span></Link></Menu.Item>
+              <Menu.Item key="table" ><Link to="/table"><span><Icon type="laptop" />Element Table</span></Link></Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout>
+            <Content>
+              <Switch>
+                <Route exact path="/table" component={AsyncElementTable}/>
+                <Route exact path="/city" component={AsyncCity}/>
+                <Route exact path="/monitor" component={AsyncMonitor}/>
+                <Route component={AsyncNotFound}/>
+              </Switch>
+            </Content>
+          </Layout>
+        </Layout>
       </Router>
     );
   }
