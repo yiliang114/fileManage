34e729f6f79de3a9bc23ac2f539d27d49980a100
zhijianzhang <mrjzhang@tencent.com>
2018-05-24 00:14:08 +0800

    :fire: upload

--- fileManage_web/src/containers/Upload/index.js
+++ fileManage_web/src/containers/Upload/index.js
@@ -1,9 +1,65 @@
 import React from 'react'
+import { Layout,Form, Icon, Input, Button, Checkbox } from 'antd';
+import {inject, observer} from 'mobx-react'
 
-const Upload  = () => {
-  return <div>
-    Upload
-  </div>
+const FormItem = Form.Item;
+const { Header, Content, Footer } = Layout;
+
+class UploadForm extends React.Component {
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    
+        return (
+          <Layout>
+            <Content style={{ padding: '0 50px' }}>
+              <input type="file" webkitdirectory="true" directory="true" />  
+              <input type="file" />  
+              <Form onSubmit={this.handleSubmit} className="login-form">
+              <FormItem>
+                {getFieldDecorator('userName', {
+                  rules: [{ required: true, message: 'Please input your username!' }],
+                })(
+                  <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+                )}
+              </FormItem>
+              <FormItem>
+                {getFieldDecorator('password', {
+                  rules: [{ required: true, message: 'Please input your Password!' }],
+                })(
+                  <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                )}
+              </FormItem>
+              <FormItem>
+                {getFieldDecorator('remember', {
+                  valuePropName: 'checked',
+                  initialValue: true,
+                })(
+                  <Checkbox>Remember me</Checkbox>
+                )}
+                <a className="login-form-forgot" href="">Forgot password</a>
+                <Button type="primary" htmlType="submit" className="login-form-button">
+                  Log in
+                </Button>
+                Or <a href="">register now!</a>
+              </FormItem>
+            </Form>
+            </Content>
+          </Layout>
+        );
+  }
+
 }
+
+const Upload = Form.create()(UploadForm);
 
 export default Upload
