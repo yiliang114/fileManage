f01534d71368b7fec2df67e5e267c0083c1c099f
zhijianzhang <mrjzhang@tencent.com>
2018-06-05 23:47:23 +0800

    :fire: upload imgs to qiniu

--- fileManage_web/src/services/request.js
+++ fileManage_web/src/services/request.js
@@ -6,8 +6,8 @@ const request = (api, params) => {
       .post(api, params)
       .then(resp => resp.data)
       .then(resp => {
-        // if (resp.code === 401 && resp.url) {   location.href = resp.url; } else if
-        // (resp.code === 403 && resp.info) {   message.info(resp.info); }
+        if (resp.code === 401 && resp.url) {   location.href = resp.url; } 
+        else if(resp.code === 403 && resp.info) {   message.info(resp.info); }
         return resp;
       })
       .catch(error => {
