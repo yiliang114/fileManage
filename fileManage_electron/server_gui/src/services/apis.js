e8291413be2f9d7fe5001b98da16eecce1d1a696
zhijianzhang <mrjzhang@tencent.com>
2018-06-16 00:03:19 +0800

    :construction: server electron api

--- fileManage_electron/server_gui/src/services/apis.js
+++ fileManage_electron/server_gui/src/services/apis.js
@@ -1,43 +1,11 @@
-// http://localhost:8080/api/element/elements
-import request from './request'
 import axios from 'axios'
 
-
-export const getElements = () => {
-  return request('/element/api/elements')
-}
-
-// http://localhost:8080/cmd/clientWork
-// 传输文件
-export const clientWork = (SERVER_IP,SERVER_PORT,params) => {
-  // return request(`/cmd/clientWork?SERVER_IP=${SERVER_IP}&SERVER_PORT=${SERVER_PORT}&fileSrc=${fileSrc}`)
-  // return request('cmd/clientWork',{
-  //   SERVER_IP,
-  //   SERVER_PORT,
-  //   ...params
-  // })
+export const serverStart = (serverPort,receiveFolder) => {
 
   return axios
-    .post('cmd/clientWork', {
-      serverIp: SERVER_IP,
-      serverPort: SERVER_PORT,
-      ...params
-    })
-    .then(resp => resp.data)
-    .then(resp => {
-      return resp;
-    })
-    .catch(error => {
-      console.error('请求出错，请重试')
-    });
-}
-
-export const monitorClientWork = (SERVER_IP,SERVER_PORT,params) => {
-  return axios
-    .post('cmd/monitorClientWork', {
-      serverIp: SERVER_IP,
-      serverPort: SERVER_PORT,
-      ...params
+    .post('cmd/serverStart', {
+      serverPort,
+      receiveFolder
     })
     .then(resp => resp.data)
     .then(resp => {
